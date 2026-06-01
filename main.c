b6b4e55ebdeecfd5bccd2c8fa6d69a6c2da0f29c
Matěj Barcal <barcama24@sps-prosek.cz>
2026-06-01 22:36:35 +0200

    Add createFileA and some error handlers

--- main.c
+++ main.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <windows.h>
+#include <winbase.h>
+
 
 
 int main(void) {
-    while (true) {
-        char interface[1024];
-        printf("Vas dotaz\n");
-        fgets(interface, sizeof(interface), stdin);
-        interface[strlen(interface) - 1] = '\0';
-        if (strcmp(interface, "q") == 0) {
-            break;
-        }
+    printf("Enter a directory to be watched\n");
+    char dir[1024];
+    fgets(dir, sizeof(dir), stdin);
+    dir[strlen(dir) - 1] = '\0';
+    HANDLE file = CreateFileA(dir, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (file == INVALID_HANDLE_VALUE){
+        DWORD error = GetLastError();
+        if (error == ERROR_FILE_NOT_FOUND){}
+        return error;
     }
+    printf("Ahoj jde to celkem");
+
+
     return 0;
 }
