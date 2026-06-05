69d654dcbfe82dd19733d4a273ed756afaab337c
Matěj Barcal <barcama24@sps-prosek.cz>
2026-06-05 22:12:51 +0200

    add system check, if the OS is windows

--- main.c
+++ main.c
@@ -7,6 +7,12 @@
 
 
 int main(void) {
+
+    #ifndef _WIN32 || _WIN64
+        printf("Tento system nelze provozovat z duvodu nepodporovaneho systemu\n");
+        return -1;
+    #endif
+
     printf("Enter a directory to be watched\n");
     char dir[1024];
     fgets(dir, sizeof(dir), stdin);
