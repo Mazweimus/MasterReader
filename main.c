c0480d9f92ff8256ea58a92426be7ecbcea16cc0
Matěj Barcal <barcama24@sps-prosek.cz>
2026-06-09 22:25:16 +0200

    Add temporary createFileW and prepare ReadDirectoryChnages

--- main.c
+++ main.c
@@ -13,20 +13,20 @@ int main(void) {
         return -1;
     #endif
 
-    printf("Enter a directory to be watched\n");
     char dir[1024];
+    printf("Enter a directory to be watched\n");
     fgets(dir, sizeof(dir), stdin);
     dir[strlen(dir) - 1] = '\0';
-    HANDLE file = FindFirstChangeNotificationA(dir, TRUE, FILE_NOTIFY_CHANGE_FILE_NAME);
-    if (file == INVALID_HANDLE_VALUE){
-        DWORD error = GetLastError();
-        return error;
-    }
+    HANDLE openDirectory = CreateFileW(dir, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    // HANDLE file = FindFirstChangeNotificationA(dir, TRUE, FILE_NOTIFY_CHANGE_FILE_NAME); //Create a handler to change the specific part of it
+    BOOL directoryChanges = ReadDirectoryChanges(file, );
+    
     printf("Iniciace %s byla uspesne udelana\n", dir);
     while (true){
         DWORD hitter = WaitForSingleObject(file, INFINITE);
         if (hitter == WAIT_OBJECT_0) {
             printf("The hitter was hitted\n");
+            printf("%s byl nalezen\n", file);
             FindNextChangeNotification(file);
         }
     }
