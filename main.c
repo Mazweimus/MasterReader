837539896e99ed2c528b186bb2e83b7f03b6d4e1
Matěj Barcal <barcama24@sps-prosek.cz>
2026-06-28 11:29:17 +0200

    add while loop with FILE_NOTIFY_INFORMATION structure

--- main.c
+++ main.c
@@ -8,26 +8,36 @@
 
 int main(void) {
 
-    #ifndef _WIN32 || _WIN64
-        printf("Tento system nelze provozovat z duvodu nepodporovaneho systemu\n");
-        return -1;
+    #ifndef _WIN32
+        #error "Tento system nelze provozovat z duvodu nepodporovaneho systemu"
     #endif
 
-    LPCWSTR dir[1024];
+    wchar_t memoryDir[1024];
+    LPCWSTR dir = memoryDir;
     printf("Enter a directory to be watched\n");
-    fgets(dir, sizeof(dir), stdin);
-    dir[strlen(dir) - 1] = '\0';
+    fgetws(memoryDir, sizeof(memoryDir), stdin);
+    memoryDir[wcslen(memoryDir) - 1] = '\0';
     HANDLE openDirectory = CreateFileW(dir, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OVERLAPPED, NULL);
     BYTE buffer[4096]; // kdyz je to pole, tak je to vzdy pointer na ten prvni prvek
     DWORD dwBufferReturn = 0;
     OVERLAPPED ov = {0};
     ov.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL); // manual reset event
+    ReadDirectoryChangesW(openDirectory, buffer, sizeof(buffer), TRUE, FILE_NOTIFY_CHANGE_LAST_WRITE, &dwBufferReturn, &ov, NULL);
     while (true) {
-        BOOL directoryChanges = ReadDirectoryChangesW(openDirectory, buffer, sizeof(buffer), TRUE, FILE_NOTIFY_CHANGE_LAST_WRITE, &dwBufferReturn, NULL, NULL);
-        if (directoryChanges == 0) {
-            printf("Nezdarilo se");
-        } else {
-            printf("Neco se znemnio");
+        DWORD wait = WaitForSingleObject(ov.hEvent, INFINITE);
+        if (wait == WAIT_OBJECT_0) {
+            GetOverlappedResult(ov.hEvent, &ov, &dwBufferReturn, TRUE);
+            ResetEvent(ov.hEvent);
+            FILE_NOTIFY_INFORMATION *fni = (FILE_NOTIFY_INFORMATION *)buffer;
+            if (fni->Action == FILE_ACTION_ADDED) {
+                printf("Added file\n");
+            } else if (fni->Action == FILE_ACTION_REMOVED) {
+                printf("File removed\n");
+            } else if (fni->Action == FILE_ACTION_MODIFIED) {
+                printf("Change was spotted\n");
+            } else if (fni->Action == FILE_ACTION_RENAMED_NEW_NAME) {
+                printf("Name renamed\n");
+            }
         }
     }
 }
