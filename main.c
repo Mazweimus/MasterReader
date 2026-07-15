831dcd7009e25e4d92b1cd4c5e7a095c9e42cd0a
Matěj Barcal <barcama24@sps-prosek.cz>
2026-07-15 18:48:24 +0200

    printing a stage in directory

--- main.c
+++ main.c
@@ -14,30 +14,39 @@ int main(void) {
 
     wchar_t memoryDir[1024];
     LPCWSTR dir = memoryDir;
-    printf("Enter a directory to be watched\n");
-    fgetws(memoryDir, sizeof(memoryDir), stdin);
+    wprintf(L"Enter a directory to be watched\n");
+    if(fgetws(memoryDir, 1024, stdin)==NULL) {
+        wprintf(L"Couldn't read memory directory\n");
+        return 1;
+    }
     memoryDir[wcslen(memoryDir) - 1] = '\0';
     HANDLE openDirectory = CreateFileW(dir, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OVERLAPPED, NULL);
+    if (openDirectory == INVALID_HANDLE_VALUE) {
+        wprintf(L"Couldn't open memory directory\n");
+        return 1;
+    }
     BYTE buffer[4096]; // kdyz je to pole, tak je to vzdy pointer na ten prvni prvek
     DWORD dwBufferReturn = 0;
     OVERLAPPED ov = {0};
     ov.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL); // manual reset event
-    ReadDirectoryChangesW(openDirectory, buffer, sizeof(buffer), TRUE, FILE_NOTIFY_CHANGE_LAST_WRITE, &dwBufferReturn, &ov, NULL);
+    ReadDirectoryChangesW(openDirectory, buffer, sizeof(buffer), TRUE, FILE_NOTIFY_CHANGE_LAST_WRITE | FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_DIR_NAME | FILE_NOTIFY_CHANGE_SIZE | FILE_NOTIFY_CHANGE_CREATION, &dwBufferReturn, &ov, NULL);
     while (true) {
+        ResetEvent(ov.hEvent);
+        printf("We are seeking for any changes\n");
         DWORD wait = WaitForSingleObject(ov.hEvent, INFINITE);
         if (wait == WAIT_OBJECT_0) {
-            GetOverlappedResult(ov.hEvent, &ov, &dwBufferReturn, TRUE);
-            ResetEvent(ov.hEvent);
+            GetOverlappedResult(openDirectory, &ov, &dwBufferReturn, TRUE);
             FILE_NOTIFY_INFORMATION *fni = (FILE_NOTIFY_INFORMATION *)buffer;
             if (fni->Action == FILE_ACTION_ADDED) {
                 printf("Added file\n");
             } else if (fni->Action == FILE_ACTION_REMOVED) {
                 printf("File removed\n");
-            } else if (fni->Action == FILE_ACTION_MODIFIED) {
-                printf("Change was spotted\n");
             } else if (fni->Action == FILE_ACTION_RENAMED_NEW_NAME) {
                 printf("Name renamed\n");
+            } else if (fni->Action == FILE_ACTION_MODIFIED) {
+                printf("Change was spotted\n");
             }
         }
+        ReadDirectoryChangesW(openDirectory, buffer, sizeof(buffer), TRUE, FILE_NOTIFY_CHANGE_LAST_WRITE | FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_DIR_NAME | FILE_NOTIFY_CHANGE_SIZE | FILE_NOTIFY_CHANGE_CREATION, &dwBufferReturn, &ov, NULL);
     }
 }
